56e9503a8896d5546098bc267132dd46eb27d3bd
Akram, khan <Akram.khan200@gmail.com>
2024-01-07 12:10:13 +0530

    anisble

--- seed-jobs/a_folder.groovy
+++ seed-jobs/a_folder.groovy
@@ -1,7 +1,8 @@
 multibranchPipelineJob('CI-Pipelines/cart') {
     branchSources {
         github {
-            id('3535352323') // IMPORTANT: use a constant and unique identifier
+            id('23232323') // IMPORTANT: use a constant and unique identifier
+            scanCredentialsId('GitHub')
             repository('https://github.com/akramsalesforce/cart.git')
         }
     }
